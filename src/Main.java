b07c81d64b4523e6c2649de69775eb5650ed7eed
ryandvl <contatoryandvl55@gmail.com>
2024-05-15 00:44:04 -0300

    feat: edit input & press ESC to change mode

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,78 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Main extends JFrame {
-    private JPanel panel1;
+    private JPanel MainPanel;
+    private JTextArea TextInput;
 
-    public Main() {
+    public Main(
+            boolean undecorated,
+            int x,
+            int y,
+            int width,
+            int height,
+            String value
+    ) {
         setTitle("Quote");
+        setUndecorated(undecorated);
+        setContentPane(MainPanel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300, 200);
+        setMinimumSize(new Dimension(250, 150));
+        setSize(width, height);
         setLocationRelativeTo(null);
+        if(x != -1 && y != -1) {
+            setLocation(x, y);
+        }
         setVisible(true);
+        setAlwaysOnTop(true);
+
+        TextInput.setText(value);
+        TextInput.setEditable(!undecorated);
+        TextInput.setFocusable(!undecorated);
+
+        var keyListener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+
+                if(keyCode == 27) {
+                    dispose();
+
+                    var location = getLocation();
+                    var size = getSize();
+
+                    new Main(
+                            !undecorated,
+                            location.x,
+                            location.y,
+                            size.width,
+                            size.height,
+                            TextInput.getText()
+                    );
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        };
+
+        addKeyListener(keyListener);
+        TextInput.addKeyListener(keyListener);
     }
 
     public static void main(String[] args) {
-        new Main();
+        new Main(
+                false,
+                -1,
+                -1,
+                250,
+                150,
+                ""
+        );
     }
 }
